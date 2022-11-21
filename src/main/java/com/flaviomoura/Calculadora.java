4db6d659bfd1006be218fcc178c1695527d0e9e1
flavio <flaviomouracar7800@gmail.com>
2022-11-21 00:23:37 -0400

    Criado um teste na classe calculadoraTeste para verificar se a classe realmente foi instanciada, na classe Calculadora foi adicionado um atributo instancia, que e iniciado com true quando a classe e instanciada, junto com ela foi adicionado um método construtor para isso e um método get para busca do valor.

--- src/main/java/com/flaviomoura/Calculadora.java
+++ src/main/java/com/flaviomoura/Calculadora.java
@@ -1,6 +1,17 @@
 package com.flaviomoura;
 
 public class Calculadora {
+
+    private boolean instancia;
+
+    public Boolean getInstancia(){
+        return this.instancia;
+    }
+
+    public Calculadora(){
+        this.instancia = true;
+    }
+
     public int soma(int parcela1, int parcela2){
         return parcela1 + parcela2;
     }
--- src/test/java/com/flaviomoura/CalculadoraTeste.java
+++ src/test/java/com/flaviomoura/CalculadoraTeste.java
@@ -1,15 +1,23 @@
 package com.flaviomoura;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class CalculadoraTeste {
+    Calculadora calculadora = new Calculadora();
+
+    @Test
+    @DisplayName("A Classe deve ser instanciada")
+    void verificaClasseInstanciada(){
+        assertTrue(calculadora.getInstancia());
+    }
+
     @Test
     @DisplayName("Soma de dois inteiros")
     void deveriaSomarInteiros(){
-        Calculadora calculadora = new Calculadora();
         assertEquals(2, calculadora.soma(1,1));
     }
     
