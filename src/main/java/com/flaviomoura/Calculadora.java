8089efcbf0b2c2233ffd7771cf5943e863b3a053
flavio <flaviomouracar7800@gmail.com>
2022-11-21 00:28:23 -0400

    Implementado na classe Calculadora os métodos subtracao, multiplicacao e divisao.

--- src/main/java/com/flaviomoura/Calculadora.java
+++ src/main/java/com/flaviomoura/Calculadora.java
@@ -15,4 +15,16 @@ public class Calculadora {
     public int soma(int parcela1, int parcela2){
         return parcela1 + parcela2;
     }
+
+    public int subtracao(int parcela1, int parcela2){
+        return parcela1 - parcela2;
+    }
+
+    public int multiplicacao(int parcela1, int parcela2){
+        return parcela1 * parcela2;
+    }
+
+    public int divisao(int parcela1, int parcela2){
+        return parcela1 / parcela2;
+    }
 }
